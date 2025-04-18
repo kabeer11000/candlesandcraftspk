072373355852e6f02aba99e56a4ea6b3a6bb8174
Kabeer Jaffri <kabeer11000@gmail.com>
2025-04-18 06:25:35 +0500

    added adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
+
   "plugins": [
     {
         "name": "@astrojs/ts-plugin"
     }
-]
-}); 
+],
+
+  adapter: vercel()
+});
